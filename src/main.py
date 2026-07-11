0ec0470f853705e68afb4e038428aa7dd93d620d
Sam Zhong <samzhongnz@gmail.com>
2026-07-12 11:45:31 +1200

    Add ETL command line arguments

--- src/main.py
+++ src/main.py
@@ -1,20 +1,50 @@
 from extract import read_transactions_csv
 from transform import transform_transaction_list
 from load import create_database, insert_transactions
+import argparse
+
+def parse_args():
+    parse = argparse.ArgumentParser(
+        description="Run banking transaction ETL pipeline"
+    )
+
+    parse.add_argument(
+        "--input",
+        default="data/raw/transactions.csv",
+        help="Path to the input transaction csv file"
+    )
+
+    parse.add_argument(
+        "--database",
+        default="data/output/transactions.db",
+        help="Path to the output of SQLite database"
+
+    )
+
+    parse.add_argument(
+        "--schema",
+        default="sql/create_tables.sql",
+        help="Path to the SQL schema file."
+    )
+
+    return parse.parse_args()
 
 def main()->None:
 
-    transactions = read_transactions_csv("./data/raw/transactions.csv")
+    args = parse_args()
+
+
+    transactions = read_transactions_csv(args.input)
 
     transformed_list = transform_transaction_list(transactions)
 
 
     create_database(
-        "data/output/transactions.db",
-        "sql/create_tables.sql"
+        args.database,
+        args.schema,
     )
 
-    insert_transactions("data/output/transactions.db", transformed_list)
+    insert_transactions(args.database, transformed_list)
     print("Transactions inserted successfully.")
 
     print(f"Read {len(transactions)} transactions from CSV.")
