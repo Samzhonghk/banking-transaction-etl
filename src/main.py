2a802d5fea02d8e0375f9a923fbd6c0ab7f48bd3
Sam Zhong <samzhongnz@gmail.com>
2026-07-11 18:24:00 +1200

    Clean up ETL main flow

--- src/main.py
+++ src/main.py
@@ -2,29 +2,23 @@ from extract import read_transactions_csv
 from transform import transform_transaction_list
 from load import create_database, insert_transactions
 
-transactions = read_transactions_csv("./data/raw/transactions.csv")
+def main()->None:
 
-print(transactions)
-print(len(transactions))
+    transactions = read_transactions_csv("./data/raw/transactions.csv")
 
-transformed_list = []
-transformed_list = transform_transaction_list(transactions)
+    transformed_list = transform_transaction_list(transactions)
 
 
-create_database(
-    "data/output/transactions.db",
-    "sql/create_tables.sql"
-)
+    create_database(
+        "data/output/transactions.db",
+        "sql/create_tables.sql"
+    )
 
-insert_transactions("data/output/transactions.db", transformed_list)
-print("Transactions inserted successfully.")
-# for t in transactions:
-#     transformed_transaction = transform_transaction(t)
-#     transformed_list.append(transformed_transaction)
+    insert_transactions("data/output/transactions.db", transformed_list)
+    print("Transactions inserted successfully.")
 
-# print("------------------------------")
-# print(transformed_list)
-# print("CURRENCY-------"+transformed_list[0]["currency"])
-# print(type(transformed_list[0]["amount"]))
+    print(f"Read {len(transactions)} transactions from CSV.")
+    print(f"Inserted {len(transformed_list)} transactions into SQLite.")
 
-# print("data created successfully")
+if __name__ == "__main__":
+    main()
